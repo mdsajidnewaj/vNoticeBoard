b51fb778b183fb7325d48d0a1af884535873dde7
Md Sajid Newaj <mdsajidnewaj@gmail.com>
2017-02-05 20:53:34 +0600

    initial commit

---
+++ main/java/sam/springframework/VNoticeBoardApplication.java
@@ -0,0 +1,12 @@
+package sam.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VNoticeBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VNoticeBoardApplication.class, args);
+	}
+}
